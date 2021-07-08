cb1353a8ce7a5494023d614ef0ada19a1aca589e
hbowlby1 <hrbowlby@gmail.com>
2021-07-08 15:04:58 -0700

    changed main screen and added date (check logcat for format)

--- app/src/main/java/com/example/projectcars/MainActivity.java
+++ app/src/main/java/com/example/projectcars/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.projectcars;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // sets up the display date in the main activity
+        mDisplayDate = (TextView) findViewById(R.id.getDate);
+
+        // gets the current date in the format yyyy/mm/dd
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                //using day of month since there are different amount of days in a month
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                //creates the date picker with a theme
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year, month, day);
+                //sets the background color to be transparent
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                //shows the datepicker
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                Log.d(TAG, "onDateSet: date; " + month + "/" + dayOfMonth + "/" + year);
+            }
+        };
+
     }
 }
